6780ab1776ba9f11978587b7d4c3e0de53c36f73
Luciano <lramosalves37@gmail.com>
2023-09-14 18:34:42 -0300

    velocimetro_01

---
+++ condicao/index.js
@@ -0,0 +1,11 @@
+
+var vel = 51
+console.log(`a velocidade é ${vel} km/h`)
+if (vel > 50){
+    console.log(`Você ultrapassou a velocidade permitida.MULTADO!`) 
+}
+else{
+    console.log(`BOA VIAGEM!`)  
+}
+console.log(`ATENCÃO NA PISTA, USE O CINTO DE SEGURANÇA!`)
+//sempre quando exercutar esse programa a pessoa vai ser multado, temos q usar as condições
