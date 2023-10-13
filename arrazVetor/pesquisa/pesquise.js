afc50fb151de9be9135b7b42ed652e1508aa48bd
Luciano <lramosalves37@gmail.com>
2023-10-13 20:41:28 -0300

    reduce

--- arrazVetor/pesquisa/pesquise.js
+++ arrazVetor/pesquisa/pesquise.js
@@ -46,3 +46,32 @@ bpesquisar.addEventListener("click", (evt)=>{
     
 })*/
 
+ /*//! reduz----------------posso reduzir um arraz..com minha regras...seja somando todos ...etc..class=''
+ele soma o anterior mais o atual... 
+
+let p_array = document.getElementById("array");
+let res = document.getElementById("outres");
+let breduzir = document.getElementById("reduzir");
+
+let elementos_array= [1,2,3,4];
+
+// passo os elementos para meu elemento de saida
+
+p_array.textContent = "[" + elementos_array + "]";
+//? let dobro = []..... poderia twr isso 
+
+breduzir.addEventListener("click", (evt)=>{
+    o res pega o resultado
+   res.innerHTML = elementos_array.reduce((anterior , atual, posterior)=>{
+    //? dobro.push(atual* 2) adiciono o atual * 2 no arry dobro
+            return atual + anterior
+    })
+
+    //? res.innerHTML += dobro
+    
+})
+vamos la.. especifiqui.. pra ele somar o atual mais o anterior estao..o atual é 0.pq mão fez nenhuma soma Ainda ..ai 0 o anteiror mais o atual 1 ..dar 1... agora o anetrior 1 mais o atual 2..agora o anetrior 3 mais o atual 3..agora o anetrior 6 mais o atual 4
+obs: eu escolhe essa regra .. mas poderia ser outra
+
+//* posso fazer..soma somente dos numeros pares...ou só dos maiores de 18.. enfim..*/
+
