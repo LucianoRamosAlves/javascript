53aa5fe7bd46c5eb74e44db8e541a0c0afb2009d
Luciano Ramos <lramosalves37@gmail.com>
2024-04-15 05:23:38 -0300

    js.objetos-globais

---
+++ objetos/globais/index.js
@@ -0,0 +1,6 @@
+//| são objetos reservados ex; Number("44") trasnforma a string 44 em numero, e tem algumas funçãos ex; Number.integer(4.5) ver se o numero é interiro
+
+//? tem o date()
+//? math , trabalhar com operação matematica ex; math.pow(2,10) 2 elevado a 10 , math.pi , valor de pi etc.
+
+//* posso.. lista = Array("a", "b", "c") .. usando o Array(),, o mesmo para Object()
