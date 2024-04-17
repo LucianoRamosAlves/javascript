77b278bb00629b719499a166718fb990c5bbd1e5
Luciano Ramos <lramosalves37@gmail.com>
2024-04-17 09:00:30 -0300

    js-operadores-spreed

--- operadores/spreed/index.js
+++ operadores/spreed/index.js
@@ -1,4 +1,34 @@
+//* espalha ou separa um objeto ou array
 
+//@ ex; ola mundo .. separado o l a m u n d o
+
+//| usando para clonar objetos , tranformar string em array , fazer manipulação sem afetar o original
+const towns = ['Prontera', 'Izlude', 'Payon', 'Alberta', 'Geffen', 'Morroc']
+
+console.log(towns)
+console.log(...towns)
+console.log(...towns[0]) //* pego só o ,, p r o n t e r a 
+
+const townsCopy = towns
+
+townsCopy.pop()
+townsCopy.pop()
+townsCopy.push('Juno')
+
+console.log({ towns, townsCopy }) //| veja que modifica o array original
+
+const townsClone = [...towns] //* pego o towns e clono , jogo no tomnsclone
+
+townsClone.push('Aldebaran') //| to dando push apenas no novo array
+
+console.log({ towns, townsCopy, townsClone })
+
+const townsObj = { ...towns } //* aqui espalha um objetos
+const townsObjClone = { ...townsObj }
+
+townsObjClone.test = 'Test' //* passo o nome teste só no clonoe
+
+console.log({ townsObj, townsObjClone })
 //------------- para copiar um array
 let n1 = [10,20,]
 let n2 = [40,50,60]
