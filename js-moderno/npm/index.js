d9bd498b18ff30ccd1739f1e69f54f51f338040d
Luciano Ramos <lramosalves37@gmail.com>
2024-04-18 10:21:51 -0300

    js-moderno-npm

---
+++ js-moderno/npm/index.js
@@ -0,0 +1,22 @@
+//* vou na pasta do projeto e inicio com npm--init
+//* depois vai no site npm e baixa um module
+//* pra intalar npm 
+//* baixa os modules
+
+const lodash = require('lodash');
+
+console.log(lodash.isArray([])) //* esse module verifica se é array , como tem [] então ele retorna true
+console.log(lodash.kebabCase("Luciano Ramos Alves")) //*separa por tracços o nome
+
+//@ foi baixado um monte de função , uma biblioteca com monte de função
+
+//* npm install --save-dev eslint uso esse codigo, pego um module só em quanto eu deselvo algo, depois ele é apagado, para não pesar mais
+
+//* npm install --global sass  funciona em qualquer lugar
+
+//! .jsnon
+
+//| .json é o formato do arquivo, é um arquivo de texto com algumas regras .. "para nomear as chaves"
+//| trabalhamos com string number boolean e nul
+//| usu virgula pra separar as propriedades
+//| é usado para troca de dados em diferentes sistemas com linguaguem diferente
