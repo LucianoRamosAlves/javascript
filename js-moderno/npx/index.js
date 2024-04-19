d72bddd6e6532185c6934bd2db38a974d8946c7a
Luciano Ramos <lramosalves37@gmail.com>
2024-04-19 09:01:23 -0300

    js-moderno/npx

---
+++ js-moderno/npx/index.js
@@ -0,0 +1,7 @@
+//| é o comando para executar os pacotes, as bibliotecas que instalei pela linha de comando
+//|nem todos os pacotes vai ter executaveis, tem que ver a documentação ou cria um arquivo bin. com um js
+//| coloco  npx hello-world-npm
+//| usu pra executar um funçao ou algum dado, automatização etc.. 
+//| e o script , crio um atalho, um comando meu para executar uma coisa
+//| vou em package.json , script , crio o nome do comando e o comando em se
+//| para rodar npm run nome do scrit 
