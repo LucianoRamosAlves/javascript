b8eff292b3af62ac2f44ca366fe4af39083de906
Luciano Ramos <lramosalves37@gmail.com>
2024-04-20 17:39:33 -0300

    js-moderno/projeto-bas/

---
+++ js-moderno/projeto-bas/src/index.js
@@ -0,0 +1,8 @@
+// src/index.js
+
+import './styles/index.css'
+
+const hello = () => console.log(`It works!`) 
+//* estou usando uma sitaxe moderna para mostrar a trasnformação no babel
+
+hello() 
