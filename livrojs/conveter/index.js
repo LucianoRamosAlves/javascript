caaf66a1d2473819436cba5756dabe11c97b30b2
Luciano Ramos <lramosalves37@gmail.com>
2024-04-27 21:34:30 -0300

    js- livrojs/

---
+++ livrojs/conveter/index.js
@@ -0,0 +1,8 @@
+let a = Number("10") // converter string em number
+let b = 20.5
+
+console.log(a  + b)
+
+// verifica se o numero é inteiro
+console.log(Number.isInteger(a)) 
+console.log(Number.isInteger(b))
